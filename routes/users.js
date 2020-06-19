b8b5a15448065e72683f5fd79e1d235ec181a7c9
Rakesh Rankawat <rakesh.rankawat@outlook.com>
2020-06-19 12:45:28 +0530

    users route

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,29 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
+const User = require('../models/User');
 
 // @route    POST /users
 // @des      Register user & get token
 // @access   Public
-router.post('/', (req, res) => {
-  res.send('Register a user');
-});
+router.post(
+  '/',
+  [
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Email is required').isEmail(),
+    check(
+      'password',
+      'Please enter a password with 6 or more characters'
+    ).isLength({ min: 6 }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    res.send('passed');
+  }
+);
 
 module.exports = router;
