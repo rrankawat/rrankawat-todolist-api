0a5228b2e80e2bad25a859c76abec3ac94074a15
Rakesh Rankawat <rakesh.rankawat@outlook.com>
2020-06-29 21:51:17 +0530

    cors lib

--- middleware/auth.js
+++ middleware/auth.js
@@ -8,8 +8,6 @@ module.exports = (req, res, next) => {
     return res.status(401).json({ msg: 'No token, authorization denied' });
   }
 
-  res.header('Access-Control-Allow-Origin', '*');
-
   try {
     const decoded = jwt.verify(token, config.get('jwtSecret'));
 
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -9,6 +10,9 @@ connectDB();
 
 // Init Middleware
 app.use(express.json({ extended: false }));
+
+// CORS
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Welcome to TodoList API');
